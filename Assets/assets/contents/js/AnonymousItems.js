064a29b019d84e8ccc9c70257d204e1ba34c9801
Shahroz Shakil <>
2023-11-15 18:57:05 +0500

    Project Uploaded

---
+++ Assets/assets/contents/js/AnonymousItems.js
@@ -0,0 +1,106 @@
+﻿$(window).on('load', function () {
+    var yourToken = sessionStorage.getItem('yourToken')
+
+    console.log("here ==== Authorization : Bearer " + yourToken);
+    //Common.Ajax('POST', $('#url_local').val() + "/api/Warehouse/GetAllWarehouses", "{ \"GET\": 1 }", 'json', FillGridHandler });
+
+    $.ajax({
+        url: $('#url_local').val() + "/api/AnonymousItems/GetAllAnonymousItems",
+        type: 'POST',
+        contentType: 'application/json', // Set the content type based on your API requirements
+        data: JSON.stringify({ "GET": 1 }), // Adjust the payload format based on your API
+        headers: {
+            'Authorization': 'Bearer ' + yourToken
+        },
+        success: function (data) {
+            // Handle the successful response
+            FillGridHandler(data);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            // Handle the error
+            console.log('AJAX Error: ' + textStatus, errorThrown);
+            console.log(jqXHR.responseText); // Log the response for more details
+        }
+
+    })
+    $('.datepicker').datepicker();
+});
+
+function FillGridHandler(response) {
+    console.log("=================>");
+    console.log(response);
+
+    var btnedit_ = 0;
+    var btndel_ = 0;
+    var btnadd_ = "";
+
+    Bindbody(response, 'tblAnonymous', "1", "1");
+    if (btnadd_ == "btnadd" && hasrigth == 1) {
+        $('#' + btnadd_).prop('disabled', false);
+    }
+};
+function Bindbody(json, tablename, edit_rights, delete_rights) {
+
+    //  console.log(json[0])
+    var tr;
+    var Edit_R;
+    var Delete_R;
+    $("#" + tablename).DataTable().destroy();
+    $("#" + tablename + ' tbody').empty();
+    for (var i = 0; i < json.length; i++) {
+        //console.log("================"+json.length)
+        if (edit_rights == 1) {
+            //    console.log("in edit")
+            Edit_R = "<i class=\"fas fa-edit\" style='cursor: pointer;' title=\"Edit\" onclick=Edit('" + json[i].guid + "')></i> ";
+        }
+        if (delete_rights == 1) {
+            Delete_R = "<i  class=\"far fa-trash-alt\" style='cursor: pointer;' title=\"Delete\" onclick=Delete('" + json[i].guid + "')></i>";
+        }
+
+        tr = $('<tr/>');
+        //tr.append("<td style='display: none;'>" + json[i].id + "</td>");
+        tr.append("<td  style='display: none;'>" + json[i].guid + "</td>");
+        tr.append("<td>" + json[i].itemName + "</td>");
+        tr.append("<td>" + json[i].inventoryName + "</td>");
+        tr.append("<td>" + json[i].deviceName + "</td>");
+        //tr.append("<td>" + json[i].inventoryDate + "</td>");
+        //if (json[i].scalestatus == "1") {
+        //    tr.append("<td>Active</td>");
+        //}
+        //else {
+        //    tr.append("<td>Inactive</td>");
+        //}
+        tr.append("<td style='padding-top: 5px !important'> <button class='btn btn-primary'>  <i  class=\"fa fa-edit \"  title=\"Edit\"  onclick=Edit('" + i + "')></i></button> <button class='btn btn-danger'> <i  class=\"fa fa-trash\"  title=\"Delete\"   onclick=Delete('" + json[i].scaleID + "')></i> </button></td>");
+        //tr.append("<td>" + Edit_R + Delete_R + "</td>");
+        // console.log(Edit_R)
+        $("#" + tablename + ' tbody').append(tr);
+    }
+    $("#" + tablename).DataTable(
+        {
+            "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
+        });
+}
+$('#btnadd').click(function () {
+    //$('#btnsave').text($('#hdnsave').val());
+    //$('#btnsave').prop('title', 'Save');
+    //$('#HieLevelCode').val('');
+    //$('#exampleModalLargetext').text("Level Creation");
+    //('#HieLevelDesc').val('');
+    $('#modal-lg').modal('show');
+});
+$('#cls').click(function () {
+    //$('#btnsave').text($('#hdnsave').val());
+    //$('#btnsave').prop('title', 'Save');
+    //$('#HieLevelCode').val('');
+    //$('#exampleModalLargetext').text("Level Creation");
+    //('#HieLevelDesc').val('');
+    $('#modal-lg').modal('hide');
+});
+$('#btnclose').click(function () {
+    //$('#btnsave').text($('#hdnsave').val());
+    //$('#btnsave').prop('title', 'Save');
+    //$('#HieLevelCode').val('');
+    //$('#exampleModalLargetext').text("Level Creation");
+    //('#HieLevelDesc').val('');
+    $('#modal-lg').modal('hide');
+});
---
+++ obj/Release/Package/PackageTmp/Assets/assets/contents/js/Login.js
@@ -0,0 +1,46 @@
+﻿$(document).ready(function () {
+    sessionStorage.clear();
+
+  //  Common.Ajax('POST', $('#url_local').val() + "/api/Authentication/CreateAdmin", "{}", 'json');
+
+});
+
+$("#btnLogin").click(function () {
+    // alert("alert")
+    var loginName = $("#loginName").val();
+    var pass = $("#password").val();
+    if (loginName == "") {
+        swal("Please enter a username", {
+            icon: "warning",
+        });
+    }
+    else if (pass == "") {
+        swal("Please enter a password", {
+            icon: "warning",
+        });
+    }
+    //else if (loginID == 'admin' && pass == 'pass') {
+    //    console.log("Correct password!")
+    //}
+    else {
+        //alert("in else")
+        Common.Ajax('POST', $('#url_local').val() + "/api/User/Login", "{ \"loginName\": \"" + loginName + "\",\"password\": \"" + pass + "\"}", 'json', EditRes);
+    }
+})
+
+function EditRes(response) {
+    console.log(response["token"])
+    //console.log(response.message)
+    if (response.status == '200') {
+    
+        sessionStorage.setItem("yourToken", response["token"]);
+        window.location.href = $("#front_URL").val() + "/Home/Index";
+
+    }
+    else {
+        swal("Username or Password is wrong", {
+            icon: "warning",
+        });
+        /*alert(response.message)*/
+    }
+}
