fc4e0266a4c2d23193689634687bff5d86f8e6cc
Shahroz Shakil <>
2023-11-24 18:25:27 +0500

    Dashboard Modification

--- Assets/assets/contents/js/Dashboard_charts.js
+++ Assets/assets/contents/js/Dashboard_charts.js
@@ -8,6 +8,11 @@ $(window).on('load', function () {
 
 function varianceChart() {
 
+   varianceAPI()
+
+};
+
+function varianceAPI() {
     $.ajax({
         url: $('#url_local').val() + "/api/Dashboard/GetVarianceInfo",
         type: 'GET',
@@ -29,73 +34,113 @@ function varianceChart() {
 
     })
 
-    //$.ajax({
-    //    url: $('#url_local').val() + "/api/Dashboard/GetAllInventoryItemsCountAgainstInventoryOrDevice",
-    //    type: 'POST',
-    //    contentType: 'application/json', // Set the content type based on your API requirements
-    //    data: JSON.stringify({
-    //        "Inventory": 1,
-
-
-    //    }), // Adjust the payload format based on your API
-    //    headers: {
-    //        'Authorization': 'Bearer ' + yourToken
-    //    },
-    //    success: function (data) {
-    //        // Handle the successful response
-    //        //responseFunction(data);
-    //    },
-    //    error: function (jqXHR, textStatus, errorThrown) {
-    //        // Handle the error
-    //        console.log('AJAX Error: ' + textStatus, errorThrown);
-    //        console.log(jqXHR.responseText); // Log the response for more details
-    //    }
-
-    //});
-};
+}
+var name = []
+var totalcount = []
+var count = []
+var variance = []
+var varianceadjustment = []
+var varianceadjustmenttext = []
+var chartColor = [];
+var chartLabel = [];
+var chartCount = [];
 
 function responseFunction(response) {
+   
     console.log("affasfsaf")
     console.log(response);
-    var name = []
-    var totalcount = []
-    var count = []
-    var variance = []
-    var vv = ""
-    for (var i = 0; i < response.returnTable.length; i++) {
+   
+ 
 
-        name[i] = response.returnTable[i]["name"];
-        totalcount[i] = response.returnTable[i]["totalItemsCount"];
-        count[i] = response.returnTable[i]["count"];
-        //totalcount[i] = 8
-        //count[i] = 2
-        //alert(name[i])
-        //alert(totalcount[i])
-        //alert(count[i])
-        console.log('================"' + i + '"================')
-        console.log("Total count : " + totalcount[i])
-        console.log("count : " + count[i])
-        variance[i] = (totalcount[i] + count[i]) / 2
-        console.log("a+b/2 :" + variance[i])
+    if ($("#varianceselect option:selected").text() == "By Items") {
+        for (var i = 0; i < response.returnTable.length; i++) {
 
-        //alert(variance[i])
-        variance[i] = totalcount[i] - variance[i]
-        console.log("c-b :" + variance[i])
+            name[i] = response.returnTable[i]["name"];
+            totalcount[i] = response.returnTable[i]["totalItemsCount"];
+            count[i] = response.returnTable[i]["count"];
+            //totalcount[i] = 8
+            //count[i] = 2
+            //alert(name[i])
+            //alert(totalcount[i])
+            //alert(count[i])
+            console.log('================"' + i + '"================')
+            console.log("Total count : " + totalcount[i])
+            console.log("count : " + count[i])
+            //variance[i] = (totalcount[i] + count[i]) / 2
+            variance[i] = ((count[i] - totalcount[i]) / totalcount[i]) * 100
+            varianceadjustment[i] = variance[i]
+            varianceadjustment[i] = parseFloat(varianceadjustment[i].toFixed(2));
+            if (variance[i].toString().includes('-')) {
+                varianceadjustmenttext[i] = "Negative Adjustment"
+            }
+            else {
+                varianceadjustmenttext[i] = "Positive Adjustment"
+            }
 
-        //alert(variance[i])
-        variance[i] = Math.pow((variance[i]), 2)
-        console.log("Final :" + variance[i] + " %")
-
-        //alert(variance[i])
+            variance[i] = Math.abs(variance[i])
 
 
+            //console.log("a+b/2 :" + variance[i])
+
+            ////alert(variance[i])
+            ////variance[i] = totalcount[i] - variance[i]
+            //console.log("c-b :" + variance[i])
+
+            ////alert(variance[i])
+            ////variance[i] = Math.pow((variance[i]), 2)
+            //console.log("Final :" + variance[i] + " %")
+
+            ////alert(variance[i])
+
+
+        }
+    }
+    else {
+        for (var i = 0; i < response.returnTable.length; i++) {
+
+            name[i] = response.returnTable[i]["name"];
+            totalcount[i] = response.returnTable[i]["totalItemsPrice"];
+            count[i] = response.returnTable[i]["foundItemsPrice"];
+            //totalcount[i] = 8
+            //count[i] = 2
+            //alert(name[i])
+            //alert(totalcount[i])
+            //alert(count[i])
+            console.log('================"' + i + '"================')
+            console.log("Total count : " + totalcount[i])
+            console.log("count : " + count[i])
+            //variance[i] = (totalcount[i] + count[i]) / 2
+            variance[i] = ((count[i] - totalcount[i]) / totalcount[i]) * 100
+            varianceadjustment[i] = variance[i]
+            varianceadjustment[i] = parseFloat(varianceadjustment[i].toFixed(2));
+            if (variance[i].toString().includes('-')) {
+                varianceadjustmenttext[i] = "Negative Adjustment"
+            }
+            else {
+                varianceadjustmenttext[i] = "Positive Adjustment"
+            }
+
+            variance[i] = Math.abs(variance[i])
+
+
+            //console.log("a+b/2 :" + variance[i])
+
+            ////alert(variance[i])
+            ////variance[i] = totalcount[i] - variance[i]
+            //console.log("c-b :" + variance[i])
+
+            ////alert(variance[i])
+            ////variance[i] = Math.pow((variance[i]), 2)
+            //console.log("Final :" + variance[i] + " %")
+
+            ////alert(variance[i])
+
+
+        }
     }
     //alert(variance[0])
     /* alert(variance[0])*/
-    var chartColor = [];
-    var chartLabel = [];
-    var chartCount = [];
-
+ 
     for (var i = 0; i < variance.length; i++) {
 
         chartCount[i] = variance[i];
@@ -111,11 +156,20 @@ function responseFunction(response) {
         }
     };
 
+   createChart()
+}
+var chart1 = null
+function createChart() {
     'use strict'
 
     // Current Ticket Status
     var ctx1 = document.getElementById('chartJS1').getContext('2d');
-    var chart1 = new Chart(ctx1, {
+    if (chart1 !== null) {
+
+        chart1.destroy(); // Destroy the existing chart
+    }
+
+    chart1 = new Chart(ctx1, {
         type: 'bar',
         data: {
             labels: chartLabel,
@@ -124,6 +178,7 @@ function responseFunction(response) {
             datasets: [{
                 data: chartCount,
                 backgroundColor: chartColor,
+
                 barPercentage: 0.3
                 //}, {
                 //    data: [10, 40, 30, 40, 60, 55, 45, 35, 30, 20],
@@ -145,7 +200,7 @@ function responseFunction(response) {
                     callbacks: {
                         label: function (tooltipItem, data) {
                             //debugger
-                            console.log(chartCount)
+                         //   console.log(chartCount)
                             var rrr = [];
                             rrr = chartCount;
                             //console.log(tooltipItem)
@@ -158,12 +213,18 @@ function responseFunction(response) {
                             //debugger
 
 
-                            var varianceData = variance[tooltipItem.dataIndex];
+                            var varianceData = varianceadjustment[tooltipItem.dataIndex];
+                            var varianceText = varianceadjustmenttext[tooltipItem.dataIndex];
                             var TotalCount = totalcount[tooltipItem.dataIndex];
                             var Count = count[tooltipItem.dataIndex];
                             var datasetIndex = tooltipItem.dataIndex;
+                            if ($("#varianceselect option:selected").text() == "By Items") {
+                                return [varianceText, 'Variance: ' + varianceData + '%', 'Total Items: ' + TotalCount, 'Found Items: ' + Count];
+                            }
+                            else {
+                                return [varianceText, 'Variance: ' + varianceData + '%', 'Total Items Price: ' + TotalCount, 'Found Items Price: ' + Count];
 
-                            return ['Variance: ' + varianceData + '%', 'Total Items: ' + TotalCount, 'Found Items: ' + Count];
+                            }
                             //// Get the corresponding value from the array
                             //var tooltipValue = data.datasets[datasetIndex].data[index];
 
@@ -200,5 +261,23 @@ function responseFunction(response) {
                 }
             }
         }
+
+
     });
+    return chart1;
 }
+$("#varianceselect").change(function () {
+   name = []
+   totalcount = []
+   count = []
+   variance = []
+   varianceadjustment = []
+   varianceadjustmenttext = []
+   chartColor = [];
+   chartLabel = [];
+    chartCount = [];
+    debugger
+  
+    varianceAPI()
+ 
+});
